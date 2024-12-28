8a76282b453e63188c48d549f73f375d37a648f1
aswinshub <aswin1998ply@gmail.com>
2024-12-28 15:15:33 +0530

    bulk update

--- src/routes/mto.js
+++ src/routes/mto.js
@@ -2,6 +2,8 @@ const express = require("express");
 const mtoController = require("../controllers/mtoController");
 const authVerify = require("../middlewares/authVerify");
 const mtoRoute = express.Router();
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
 
 mtoRoute.use(authVerify);
 mtoRoute.post("/", mtoController.createMto);
@@ -17,5 +19,6 @@ mtoRoute.get("/download", mtoController.downloadMtoCsv);
 mtoRoute.get("/summery/:id", mtoController.fetchSummaryByProjectId);
 
 mtoRoute.get("/summery/download/:id", mtoController.downloadSummaryByProjectId);
+mtoRoute.post("/upload", upload.single("file"), mtoController.bulkUpload);
 
 module.exports = mtoRoute;
