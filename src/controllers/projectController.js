ba743d5bd5a8b824d258c9d88498ff765973a7f7
hello-world-ttj <tijotjoseph003@gmail.com>
2025-01-13 17:12:37 +0530

    #logs

--- src/controllers/projectController.js
+++ src/controllers/projectController.js
@@ -34,12 +34,13 @@ exports.createProject = async (req, res) => {
     const workbook = xlsx.readFile(filePath, { cellDates: true });
     const sheetName = workbook.SheetNames[0];
 
+    console.log("I am in line number 37");
     const dataRows = xlsx.utils
       .sheet_to_json(workbook.Sheets[sheetName], {
         header: 1,
       })
       .slice(1);
-
+      console.log("I am in line number 43");
     const rawHeaders = xlsx.utils.sheet_to_json(workbook.Sheets[sheetName], {
       header: 1,
     })[0];
@@ -48,7 +49,7 @@ exports.createProject = async (req, res) => {
       fs.unlinkSync(filePath);
       return responseHandler(res, 400, "Excel file has no headers");
     }
-
+    console.log("I am in line number 52");
     req.body.headers = rawHeaders;
     req.body.pk = snakeCase(req.body.pk);
     req.body.issuedQty = snakeCase(req.body.issuedQty);
@@ -60,9 +61,11 @@ exports.createProject = async (req, res) => {
 
     const mtoCollectionName = await generateUniqueDigit(6);
     req.body.collectionName = `mto_${mtoCollectionName}`;
-
+    console.log("I am in line number 63");
+    
     const newProject = await Project.create(req.body);
 
+    console.log("I am in line number 67");
     if (newProject && newProject.headers) {
       const mtoSchemaDefinition = {};
 
@@ -75,18 +78,18 @@ exports.createProject = async (req, res) => {
         }
         mtoSchemaDefinition[fieldName] = { type: String };
       });
-
+      console.log("I am in line number 81");
       mtoSchemaDefinition.project = {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Project",
         required: true,
       };
-
+      console.log("I am in line number 87");
       const mtoSchema = new mongoose.Schema(mtoSchemaDefinition, {
         timestamps: true,
       });
       const MtoDynamic = mongoose.model(`mto_${mtoCollectionName}`, mtoSchema);
-
+      console.log("I am in line number 92");
       const dataToInsert = dataRows.map((row) => {
         const rowData = { project: newProject._id };
 
@@ -106,9 +109,10 @@ exports.createProject = async (req, res) => {
 
         return rowData;
       });
+      console.log("I am in line number 112");
       await MtoDynamic.insertMany(dataToInsert);
     }
-
+    console.log("I am in line number 115");
     if (newProject) {
       return responseHandler(
         res,
