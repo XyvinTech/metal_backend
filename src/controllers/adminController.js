6445135872d95b9c61d7508f8a865e23edef1d9c
aswinshub <aswin1998ply@gmail.com>
2025-02-04 10:37:05 +0530

    pk

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -257,7 +257,8 @@ exports.getAllLogs = async (req, res) => {
       return {
         ...logs,
         adminName: logs?.admin?.name || "",
-        projectName: logs?.project?.project || "Unknown/Deleted",
+        projectName: logs?.project?.project,
+        pknName: logs?.project?.pk,
       };
     });
 
